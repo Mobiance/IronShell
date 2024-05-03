0b51c1ac62d747c3c020aba35afdd2605f1a8248
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-03 21:29:11 +0530

    add a way to pass arguments to the shell

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
 
-    let command = input.trim();
+    let mut parts = input.trim().split_whitespace();
+    let command = parts.next().unwrap();
+    let args = parts;
 
     let mut child = Command::new(command)
+        .args(args)
         .spawn()
         .unwrap();
 
