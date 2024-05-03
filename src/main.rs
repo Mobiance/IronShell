125501f01dc503946d9906ad7a7242a7fd46270d
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-03 21:23:02 +0530

    add a loop to continue taking user input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use std::process::Command;
+use std::{io::{stdout, Write}, process::Command};
 
 fn main() {
+    loop {
+    println!("Type a command to run:");
+    print!("$ ");
+    stdout().flush().unwrap();
+
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
 
     let command = input.trim();
 
-    Command::new(command)
+    let mut child = Command::new(command)
         .spawn()
         .unwrap();
+
+    child.wait().unwrap();
+    }
 }
 
