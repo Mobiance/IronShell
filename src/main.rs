8d0b0caac81b4cfee76e164e93d737504eaea10d
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-03 21:19:28 +0530

    add ability to take parse input and execute command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
 }
+
