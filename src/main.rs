6796b3d4dbf323cd6a33a1b8e73568309424a358
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-03 21:34:53 +0530

    add cd command as built in command

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
-use std::{io::{stdout, Write}, process::Command};
+use std::{
+    io::{stdout, Write},
+    path::Path,
+    process::Command,
+};
 
 fn main() {
     loop {
-    println!("Type a command to run:");
-    print!("$ ");
-    stdout().flush().unwrap();
+        println!("Type a command to run:");
+        print!("$ ");
+        stdout().flush().unwrap();
 
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
 
-    let mut parts = input.trim().split_whitespace();
-    let command = parts.next().unwrap();
-    let args = parts;
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
-    let mut child = Command::new(command)
-        .args(args)
-        .spawn()
-        .unwrap();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = std::env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
 
-    child.wait().unwrap();
+                child.wait().unwrap();
+            }
+        }
     }
 }
 
